80a133ffcffc7cbc5e56ecca00b6c14e3a3e8472
hatcherCheung <1109079622@qq.com>
2022-06-21 23:12:51 +0800

    增加mockito的spy注解

--- No01_MockIto/src/test/java/com/hatcher/Demo02Test.java
+++ No01_MockIto/src/test/java/com/hatcher/Demo02Test.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import java.util.Random;
 
@@ -17,6 +18,9 @@ class Demo02Test {
     @Mock
     private Random mockRandom;
 
+    @Spy
+    private Random mockRandomSpy;
+
     @Test
     void add() {
         MockitoAnnotations.openMocks(this);
@@ -25,5 +29,15 @@ class Demo02Test {
         System.out.println(mockRandom.nextInt());
         // 看mockRandom是否执行了nextInt方法,并且执行了一次(次数可省略)
         Mockito.verify(mockRandom, Mockito.times(1)).nextInt();
+    }
+
+    @Test
+    void addSpy() {
+        MockitoAnnotations.openMocks(this);
+        //no when：627194343
+        System.out.println("no when：" + mockRandomSpy.nextInt());
+        Mockito.when(mockRandomSpy.nextInt()).thenReturn(114514);
+        //already when：114514
+        System.out.println("already when：" + mockRandomSpy.nextInt());
     }
 }
